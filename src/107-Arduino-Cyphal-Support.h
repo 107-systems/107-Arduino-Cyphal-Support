60bfeff1cadaf27b8bcda1dad40a7c9475d76be3
Alexander Entinger <consulting@lxrobotics.com>
2023-03-30 07:48:39 +0200

    Initial commit.

---
+++ src/107-Arduino-Cyphal-Support.h
@@ -0,0 +1,36 @@
+/**
+ * This software is distributed under the terms of the MIT License.
+ * Copyright (c) 2023 LXRobotics.
+ * Author: Alexander Entinger <alexander.entinger@lxrobotics.com>
+ * Contributors: https://github.com/107-systems/107-Arduino-Cyphal-Support/graphs/contributors.
+ */
+
+#ifndef INC_107_ARDUINO_CYPHAL_SUPPORT_H_
+#define INC_107_ARDUINO_CYPHAL_SUPPORT_H_
+
+/**************************************************************************************
+ * INCLUDE
+ **************************************************************************************/
+
+#include <107-Arduino-UniqueId.h>
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+namespace cyphal::support
+{
+
+/**************************************************************************************
+ * CLASS DECLARATION
+ **************************************************************************************/
+
+class UniqueId : public impl::UniqueId16 { };
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+} /* cyphal::support */
+
+#endif /* INC_107_ARDUINO_CYPHAL_SUPPORT_H_ */
